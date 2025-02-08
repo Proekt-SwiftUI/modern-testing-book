0bcf12ba385c3f0e1e1ea12e84634aadcb2b06eb
Nick Rossik <maybequantum@ntc.local>
2025-02-08 10:58:40 +0300

    fix read_book.zsh

--- read_book.zsh
+++ read_book.zsh
@@ -3,17 +3,13 @@
 # Nick Rossik
 # @wmorgue 2025
 
-log_error() {
-    echo "[ERROR] $1" >$2
-}
-
 try_open_url() {
     local URL="http://[::1]:3000"
     
     if open $URL; then
         echo "🟢 Opened $URL successfully\n"
     else
-        log_error "❌ Failed to open $URL"
+        echo "❌ Failed to open $URL"
         exit 1
     fi
 }
@@ -22,7 +18,7 @@ serve_mdbook() {
     if command -v mdbook > /dev/null; then
         exec mdbook serve
     else
-        log_error "mdbook is not installed or not in PATH"
+        echo "❌ mdbook is not installed or not in PATH"
         exit 2
     fi
 }
